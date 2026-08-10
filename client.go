bd0ab7d0bca116f30bb610e04bfcb6a52d0cb2e1
gabrielramos02 <gabrielrp02ok@outlook.com>
2026-08-10 12:08:15 +0200

    added api token auth

--- client.go
+++ client.go
@@ -18,9 +18,10 @@ const (
 type GopeedClient struct {
 	baseURL    string
 	httpClient *http.Client
+	apiToken   string
 }
 
-func NewClient(baseURL string) (*GopeedClient, error) {
+func NewClient(baseURL, apiToken string) (*GopeedClient, error) {
 	url, err := url.Parse(baseURL)
 	if err != nil {
 		return nil, fmt.Errorf("invalid base URL: %v", err)
@@ -30,12 +31,14 @@ func NewClient(baseURL string) (*GopeedClient, error) {
 	return &GopeedClient{
 		baseURL:    url.String(),
 		httpClient: httpClient,
+		apiToken:   apiToken,
 	}, nil
 }
 
 func (c *GopeedClient) GetInfo(path string) (GopeedInfo, error) {
 	var resp GopeedResponse[GopeedInfo]
 	req, _ := http.NewRequest("GET", c.baseURL+infoEndpoint, nil)
+	req.Header.Add("X-Api-Token", c.apiToken)
 	res, err := c.httpClient.Do(req)
 	if err != nil {
 		return resp.Data, fmt.Errorf("failed to get info: %v", err)
@@ -43,7 +46,7 @@ func (c *GopeedClient) GetInfo(path string) (GopeedInfo, error) {
 	defer res.Body.Close()
 	err = json.NewDecoder(res.Body).Decode(&resp)
 	if err != nil {
-		return resp.Data, fmt.Errorf("failed to decode response from GetInf from GetInfoo: %v", err)
+		return resp.Data, fmt.Errorf("failed to decode response from GetInfo: %v", err)
 	}
 	if resp.Code != 0 {
 		return resp.Data, fmt.Errorf("error from server: %s", resp.Msg)
@@ -54,6 +57,7 @@ func (c *GopeedClient) GetInfo(path string) (GopeedInfo, error) {
 func (c *GopeedClient) GetTasks() ([]GopeedTask, error) {
 	var resp GopeedResponse[[]GopeedTask]
 	req, _ := http.NewRequest("GET", c.baseURL+taskEndpoint, nil)
+	req.Header.Add("X-Api-Token", c.apiToken)
 	res, err := c.httpClient.Do(req)
 	if err != nil {
 		return resp.Data, fmt.Errorf("failed to get taks: %v", err)
@@ -72,6 +76,7 @@ func (c *GopeedClient) GetTasks() ([]GopeedTask, error) {
 func (c *GopeedClient) GetTask(taskID string) (GopeedTask, error) {
 	var resp GopeedResponse[GopeedTask]
 	req, _ := http.NewRequest("GET", c.baseURL+taskEndpoint+"/"+taskID, nil)
+	req.Header.Add("X-Api-Token", c.apiToken)
 	res, err := c.httpClient.Do(req)
 	if err != nil {
 		return resp.Data, fmt.Errorf("failed to get taks: %v", err)
@@ -86,17 +91,15 @@ func (c *GopeedClient) GetTask(taskID string) (GopeedTask, error) {
 	}
 	return resp.Data, nil
 }
-func (c *GopeedClient) CreateTask(url string) (taskid string, err error) {
+
+func (c *GopeedClient) CreateTask(
+	resolvedID string,
+	opts GopeedOptions,
+) (taskid string, err error) {
 	var resp GopeedResponse[string]
-	resolvedID, err := c.resolve(url)
-	if err != nil {
-		return "", err
-	}
 	payload := GopeedCreateTask{
-		Rid: resolvedID,
-		Opts: GopeedOptions{
-			Extra: &GopeedExtraOptions{Connections: 32},
-		},
+		Rid:  resolvedID,
+		Opts: opts,
 	}
 	jsonData, err := json.Marshal(payload)
 	if err != nil {
@@ -104,6 +107,7 @@ func (c *GopeedClient) CreateTask(url string) (taskid string, err error) {
 	}
 	reader := bytes.NewReader(jsonData)
 	req, _ := http.NewRequest("POST", c.baseURL+taskEndpoint, reader)
+	req.Header.Add("X-Api-Token", c.apiToken)
 	res, err := c.httpClient.Do(req)
 	if err != nil {
 		return "", fmt.Errorf("failed to create task: %v", err)
@@ -119,35 +123,38 @@ func (c *GopeedClient) CreateTask(url string) (taskid string, err error) {
 	return resp.Data, nil
 
 }
-func (c *GopeedClient) resolve(url string) (resolvedID string, err error) {
+
+func (c *GopeedClient) Resolve(
+	url string,
+	opts GopeedOptions,
+) (resolved GopeedResolved, err error) {
 	var resp GopeedResponse[GopeedResolved]
 	payload := GopeedResolve{
-		Req: GopeedRequest{URL: url},
-		Opts: GopeedOptions{
-			Extra: &GopeedExtraOptions{Connections: 32},
-		},
+		Req:  GopeedRequest{URL: url},
+		Opts: opts,
 	}
 	jsonData, err := json.Marshal(payload)
 	if err != nil {
-		return "", fmt.Errorf("failed to marshal payload: %v", err)
+		return resp.Data, fmt.Errorf("failed to marshal payload: %v", err)
 	}
 	reader := bytes.NewReader(jsonData)
 
 	req, _ := http.NewRequest("POST", c.baseURL+resolveEndpoint, reader)
-	res, err := http.DefaultClient.Do(req)
+	req.Header.Add("X-Api-Token", c.apiToken)
+	res, err := c.httpClient.Do(req)
 	if err != nil {
-		return resp.Data.Id, fmt.Errorf("failed to resolve resource: %v", err)
+		return resp.Data, fmt.Errorf("failed to resolve resource: %v", err)
 	}
 	defer res.Body.Close()
 
 	err = json.NewDecoder(res.Body).Decode(&resp)
 	if err != nil {
-		return resp.Data.Id, fmt.Errorf("failed to decode response from resolve: %v", err)
+		return resp.Data, fmt.Errorf("failed to decode response from resolve: %v", err)
 	}
 	if resp.Code != 0 {
-		return "", fmt.Errorf("error from server: %s", resp.Msg)
+		return resp.Data, fmt.Errorf("error from server: %s", resp.Msg)
 	}
-	return resp.Data.Id, nil
+	return resp.Data, nil
 }
 
 func (c *GopeedClient) DeleteTask(taskID string) error {
@@ -155,6 +162,7 @@ func (c *GopeedClient) DeleteTask(taskID string) error {
 	params := url.Values{}
 	params.Add("force", "true")
 	req, _ := http.NewRequest("DELETE", c.baseURL+taskEndpoint+"/"+taskID+"?"+params.Encode(), nil)
+	req.Header.Add("X-Api-Token", c.apiToken)
 	res, err := c.httpClient.Do(req)
 	if err != nil {
 		return fmt.Errorf("failed to delete task: %v", err)
